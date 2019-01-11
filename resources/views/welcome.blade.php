6e704af2710ab4aee20504f2d14dc8c0a3ac4867
afendi <afendiajip94@gmail.com>
2019-01-11 14:15:41 +0700

    yyy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,9 +85,8 @@
                 </div>
 
                 <div class="links">
-
                     <a href="https://laravel.com/docs">Documentations</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">Laracast</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
