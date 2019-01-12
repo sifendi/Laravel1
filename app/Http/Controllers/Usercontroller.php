b6878023dec95fc93b3639fd3f80abb94b49cea8
afendi <afendiajip94@gmail.com>
2019-01-12 21:18:29 +0700

    call view from controller

--- app/Http/Controllers/Usercontroller.php
+++ app/Http/Controllers/Usercontroller.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Usercontroller extends Controller
-{
-    //
+class Usercontroller extends Controller{
+    //new controller
+    public function index(){
+        return "Hello, I am User controller in function index";
+    }
+
+    public function home(){
+        return "I am home";
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('user', function () {
-    return view('view_user');
-});
+// Route::get('user', function () {
+//     return view('view_user');
+// });
+
+Route::get('user','Usercontroller@index');
